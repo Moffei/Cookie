8df327b7ae89ae46ad77df08968b213f0841af62
DSU App Academy <dsuappacademy@DSUs-MacBook-Pro-4.local>
2016-07-11 15:58:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dsuappacademy/cookie/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.dsuappacademy.cookie;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+        displayPicture();
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+        displayText();
+
+    }
+
+    public void displayPicture(){
+        imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        imageView.setImageResource(R.drawable.after_cookie);
+    }
+    public void displayText(){
+        textView = (TextView) findViewById(R.id.status_text_view);
+        textView.setText("I'm full");
+    }
+
+    
+    //Reset button code
+    public void resetButton(View view) {
+        imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        imageView.setImageResource(R.drawable.before_cookie);
+
+        textView = (TextView) findViewById(R.id.status_text_view);
+        textView.setText("I'm so hungry");
+    }
+}
